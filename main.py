32e61c256ed4b5ff977ccec6c5949c3644dc4c6d
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-03-16 20:10:30 +0100

    Test on pycharm

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import subprocess
 cmd = 'WMIC PROCESS get Caption,Commandline,Processid'
 proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
 for line in proc.stdout:
-    if line == "Discord.exe":
+    print(line)
+    if line == "b'Discord.exe":
         print('1')
+        break
     else:
         print('0')
