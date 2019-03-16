8954b36bb7261809cee5deb991fd7f5f99189c74
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-03-16 19:47:02 +0100

    Tried something with the subprocess library and psutil... However psutil is a third party library which needs to be installed separately

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-msg = "Hello, world"
+import subprocess
+#import psutil
 
-print(msg)
-
-trying = "yada yada"
+cmd = 'WMIC PROCESS get Caption,Commandline,Processid'
+proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+for line in proc.stdout:
+    if line == "Discord.exe":
+        print('1')
+    else:
+        print('0')
